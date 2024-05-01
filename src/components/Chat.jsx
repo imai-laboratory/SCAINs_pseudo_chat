753a91f8879f3172b83f8d287b7d32adcd7e53ed
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-05-01 16:02:20 +0900

    chat css

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -7,26 +7,11 @@ import Typography from "@mui/material/Typography";
 
 export const Chat = () => {
     return (
-        <ListItem alignItems="flex-start">
+        <ListItem>
             <ListItemAvatar>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             </ListItemAvatar>
-            <ListItemText
-                primary="Brunch this weekend?"
-                secondary={
-                    <React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        >
-                            Ali Connors
-                        </Typography>
-                        {" — I'll be in your neighborhood doing errands this…"}
-                    </React.Fragment>
-                }
-            />
+            <div className="chat-text sm">テスト</div>
         </ListItem>
     )
 }
