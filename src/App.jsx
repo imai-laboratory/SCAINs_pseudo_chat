1802f3729a62dac1a49d2826571c99d0dc38300f
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-05-01 15:42:15 +0900

    デフォルトchatsを表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import './assets/styles/App.css';
-import { UserStatements } from "./components/UserStatements";
 import React, { useState, useEffect } from 'react';
 import sampleData from './assets/data/sample.js'
+import {Chats, UserStatements} from "./components/index";
 
 function App() {
     const [userStatement, setUserStatements] = useState([]);
     useEffect(() => {
-            const filteredData = sampleData.filter(item => item.person === "B");
-            setUserStatements(filteredData[0].content)
-        });
+        const filteredData = sampleData.filter(item => item.person === "B");
+        setUserStatements(filteredData[0].content)
+    });
 
     return (
     <section className="app-container">
         <div className="chat-box-container">
-          <UserStatements userStatement={userStatement}/>
+            <Chats />
+            <UserStatements userStatement={userStatement} />
         </div>
     </section>
     );
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export {default as UserStatements} from './UserStatements'
+export {default as Chats} from './Chats'
