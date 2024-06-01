958faf480da3e3789a69d86fa911e08ba537db96
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-02 00:48:06 +0900

    index.jsでrouting

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Admin from './components/Admin';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  </Router>
 );
