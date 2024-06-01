51110e864eb02963a3cc6813dd3753f86a8aae42
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-02 00:51:35 +0900

    FastAPIとReact連携

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import openai
 import os
 from dotenv import load_dotenv
+import re
 
 from prompt import generate_answer
 
@@ -12,30 +15,39 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
-
-# CORS設定
-origins = [
-    "http://localhost:3000",  # React開発環境
-    # TODO: 本番環境のReactドメイン追加
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/static", StaticFiles(directory="./front/build/static"), name="static")
+
 
 class Conversation(BaseModel):
     conversation: list
     agent: str
 
 
+@app.get("/", response_class=FileResponse)
+async def serve_react_app():
+    index_file_path = "./front/build/index.html"
+    return FileResponse(index_file_path)
+
+
+@app.get("/admin", response_class=FileResponse)
+async def serve_react_app():
+    index_file_path = "./front/build/index.html"
+    return FileResponse(index_file_path)
+
+
 @app.post("/api/generate-response")
 async def generate_response(request: Conversation):
     try:
         prompt = generate_answer(request.conversation, request.agent)
+
         response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[{"role": "user", "content": prompt}],
@@ -43,9 +55,13 @@ async def generate_response(request: Conversation):
             temperature=0,
             stop=["\n"],
         )
-        return {"response": response.choices[0].message['content']}
+        content = response['choices'][0]['message']['content']
+        # 「B: 」などを除く
+        cleaned_content = re.sub(r'^.*?: ', '', content)
+        return {"response": cleaned_content}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("run:app", port=8000, reload=True)
