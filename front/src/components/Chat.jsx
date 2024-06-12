6ebbcb30d769c751919222daed1a81ee730c4501
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-12 21:00:42 +0900

    Revert "途中参加表示"
    This reverts commit 631b6381d8efe7c9f2fa96c580a83b7769690c74.

--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -12,30 +12,23 @@ function setAvatarColor(name) {
     };
 }
 
-export const Chat = ({agent, text, person, index, isScains, scains}) => {
+export const Chat = ({text, person, isScains}) => {
     const isUser = (person === "user");
     const classes = isUser ? 'right-chat' : 'left-chat'
     const textClass = isScains ? 'scains-text md text-bold' : isUser ? 'chat-user-text sm' : 'chat-text sm';
 
     return (
-        <ListItem className='chat-container'>
-            {index === scains[scains.length - 1]['index'] && (
-                <div className='agent-joined-text sm'>{agent}さんが参加しました</div>
-            )}
-            <div className={classes}>
-                <ListItemAvatar className='avatar'>
-                    {isUser ? (
-                        <Avatar alt={person} src={UserAvatar} />
-                    ) : (
-                        <Avatar {...setAvatarColor(person)} alt={person}>
-                            {person}
-                        </Avatar>
-                    )}
-                </ListItemAvatar>
-                <div className={textClass}>{text}</div>
-            </div>
+        <ListItem className={classes}>
+            <ListItemAvatar>
+                {isUser ? (
+                    <Avatar alt={person} src={UserAvatar}/>
+                ) : (
+                    <Avatar {...setAvatarColor(person)} alt={person}>{person}</Avatar>
+                )}
+            </ListItemAvatar>
+            <div className={textClass}>{text}</div>
         </ListItem>
-    );
+    )
 }
 
 export default Chat
