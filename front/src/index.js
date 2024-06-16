0b65bbf283e3e66c9e2b016c26f8529c876c035c
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-16 16:58:07 +0900

    HashRouterに変更

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin';
 import App from './App';
 
