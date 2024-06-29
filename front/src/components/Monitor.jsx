9c89041c27069853867416af4c1dcfbf5acacc8e
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-30 00:14:48 +0900

    monitor編集

--- front/src/components/Monitor.jsx
+++ front/src/components/Monitor.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
-function Monitor({ image_A, image_B, image_user, image_missing_B, isMissedListener, switchMissedImage }) {
+function Monitor({ image_A, image_B, image_user, image_missing_B, switchMissedImage }) {
     const imageUrl = switchMissedImage ? image_B : image_missing_B;
 
     return (
-        <div className="monitor-frame">
-            <div className="a-person">
-                <img src={image_A} alt={"A"} className="a-person-img"/>
-                <div>Aさん</div>
-            </div>
-            {isMissedListener ?
+        <div className="monitor-frames">
+            <div className="upper-frames">
+                <div className="a-person">
+                    <img src={image_A} alt={"A"} className="a-person-img"/>
+                    <div>Aさん</div>
+                </div>
                 <div className="a-person">
                     <img src={image_user} alt="user" className="a-person-img"/>
-                    <div>userさん</div>
-                </div> :
+                    <div>あなた</div>
+                </div>
+            </div>
+            <div className="lower-frames">
                 <div className="a-person">
                     <img src={imageUrl} alt="B" className="a-person-img"/>
                     <div>Bさん</div>
                 </div>
-            }
+            </div>
         </div>
     );
 }
