c6dc01347d77d8913ef40b91f8f17736e2eac4d0
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-30 00:49:48 +0900

    monitorのcss

--- front/src/components/Monitor.jsx
+++ front/src/components/Monitor.jsx
@@ -5,7 +5,7 @@ function Monitor({ image_A, image_B, image_user, image_missing_B, switchMissedIm
 
     return (
         <div className="monitor-frames">
-            <div className="upper-frames">
+            <div className="persons">
                 <div className="a-person">
                     <img src={image_A} alt={"A"} className="a-person-img"/>
                     <div>Aさん</div>
@@ -15,7 +15,7 @@ function Monitor({ image_A, image_B, image_user, image_missing_B, switchMissedIm
                     <div>あなた</div>
                 </div>
             </div>
-            <div className="lower-frames">
+            <div className="persons">
                 <div className="a-person">
                     <img src={imageUrl} alt="B" className="a-person-img"/>
                     <div>Bさん</div>
