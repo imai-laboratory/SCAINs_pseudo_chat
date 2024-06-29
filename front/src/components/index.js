c7d5c952e7324ad4b5e3719ce7f66449bd949013
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-30 00:14:33 +0900

    Routingの編集

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,5 +1,6 @@
 export {default as Admin} from './Admin'
 export {default as Chat} from './Chat'
 export {default as Chats} from './Chats'
+export {default as Home} from './Home'
 export {default as Monitor} from './Monitor'
 export {default as UserStatements} from './UserStatements'
--- front/src/index.js
+++ front/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Admin from './components/Admin';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
