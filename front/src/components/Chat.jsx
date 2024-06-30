2ea64098ebe9dfa58b9929d664e889ec085f5486
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-06-30 16:24:04 +0900

    userのiconに変更

--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -3,6 +3,8 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import React from "react";
 import UserAvatar from "../assets/images/no-profile.png"
+import A_icon from "../assets/images/A_icon.png"
+import B_icon from "../assets/images/B_icon.png"
 
 function setAvatarColor(name) {
     return {
@@ -17,6 +19,8 @@ export const Chat = ({agent, text, person, isMissedListener, isScains}) => {
     const isAgent = (person === agent);
     let classes;
     let textClass;
+    let icon;
+    let speaker;
 
     if (isMissedListener) {
         classes = isAgent ? 'right-chat' : 'left-chat'
@@ -26,14 +30,22 @@ export const Chat = ({agent, text, person, isMissedListener, isScains}) => {
         textClass = isScains ? 'scains-text md text-bold' : isUser ? 'chat-user-text sm' : 'chat-text sm';
     }
 
+    if (isUser) {
+        icon = UserAvatar;
+        speaker = 'あなた';
+    } else if (person === 'A') {
+        icon = A_icon;
+        speaker = 'Aさん';
+    } else if (person === agent) {
+        icon = B_icon;
+        speaker = 'Bさん';
+    }
+
     return (
         <ListItem className={classes}>
             <ListItemAvatar>
-                {isUser ? (
-                    <Avatar alt={person} src={UserAvatar}/>
-                ) : (
-                    <Avatar {...setAvatarColor(person)} alt={person}>{person}</Avatar>
-                )}
+                <Avatar alt={person} src={icon}/>
+                <div className="sm">{speaker}</div>
             </ListItemAvatar>
             <div className={textClass}>{text}</div>
         </ListItem>
