087c99fd57e1425fdf96dd75635af9de2482721c
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-07-07 15:33:09 +0900

    事前対話追加（簡易）

--- front/src/components/Admin.jsx
+++ front/src/components/Admin.jsx
@@ -3,10 +3,16 @@ import axios from "axios";
 
 function Admin({ rootURL }) {
     const [newUser, setNewUser] = useState({ name: '', email: '', role: '' });
+    const [newConversation, setNewConversation] = useState({ name: '' });
 
     const handleNewUserChange = (e) => {
         const { name, value } = e.target;
         setNewUser({ ...newUser, [name]: value });
+    };
+
+    const handleNewConversation = (e) => {
+        const { name, value } = e.target;
+        setNewConversation({ ...newConversation, [name]: value });
     };
 
     const handleUserAdd = () => {
@@ -14,6 +20,21 @@ function Admin({ rootURL }) {
             .then(response => {
                 alert('新しいユーザーが追加されました');
                 setNewUser({ name: '', email: '', role: '' });
+            })
+            .catch(error => {
+                if (error.response && error.response.status === 422) {
+                    alert('入力に誤りがあります。再確認してください。');
+                } else {
+                    alert('ユーザーの追加に失敗しました。');
+                }
+            });
+    };
+
+    const handleConversationAdd = () => {
+        axios.post(`${rootURL}/conversations/create`, newConversation)
+            .then(response => {
+                alert('対話が追加されました');
+                setNewConversation({ name: '' });
             })
             .catch(error => {
                 if (error.response && error.response.status === 422) {
@@ -50,6 +71,17 @@ function Admin({ rootURL }) {
                     placeholder="権限"
                 />
                 <button onClick={handleUserAdd}>ユーザを追加</button>
+            </div>
+            <div>
+                <h2>対話の追加</h2>
+                <input
+                    type="text"
+                    name="name"
+                    value={newConversation.name}
+                    onChange={handleNewConversation}
+                    placeholder="対話データ名"
+                />
+                <button onClick={handleConversationAdd}>対話追加</button>
             </div>
         </div>
     );
