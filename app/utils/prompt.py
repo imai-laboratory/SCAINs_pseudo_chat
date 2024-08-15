8b768da2d52770fe60784aae64ec1a7a92670f55
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-15 17:19:49 +0900

    scainsプロンプト変更

--- app/utils/prompt.py
+++ app/utils/prompt.py
@@ -32,7 +32,7 @@ def rephrase_chat_ja(dialogue, sentence):
     joined_dialogue = "\n".join(dialogue)
     prompt = (
         f"会話文中のある発言を具体的に言い換えます．\nAとBの2人の会話文が入力された後に，言い換えの対象となる発言が指定されます．\n"
-        f"指定された発言を，会話文中の言葉を使ってより具体的に言い換えます．\n\n# 会話文\n{joined_dialogue}\n\n# 言い換える発言\n"
+        f"指定された発言を，会話文中の言葉を使って指示語を含まないようにより具体的に言い換えます．\n\n# 会話文\n{joined_dialogue}\n\n# 言い換える発言\n"
         f"{sentence}\n\n# 具体的な発言\n"
     )
     return prompt
