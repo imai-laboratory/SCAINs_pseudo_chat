379214fc9c82ad3d6848d8738cfeb6e0eac9bd00
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-24 10:01:54 +0900

    i18nの導入

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import App from './App';
 import {UserProvider} from "./context/UserContext";
+import './i18n/config';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
